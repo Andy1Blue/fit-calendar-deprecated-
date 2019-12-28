a2ab99545dcba6eb2fb1613789e33947b7765617
Krzysztof Bujak <bujak.krzysztof@outlook.com>
2019-12-28 19:38:23 +0100

    Add dotenv with port

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// tslint:disable-next-line: no-var-requires
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.port);
 }
 bootstrap();
