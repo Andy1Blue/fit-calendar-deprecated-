df974d078e80d65d20ca8acf28aad4fbdbee8261
Krzysztof Bujak <bujak.krzysztof@outlook.com>
2020-02-08 21:19:28 +0100

    Add local mongodb config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,26 @@ import { TrainingsModule } from '../trainings/trainings.module';
 // tslint:disable-next-line: no-var-requires
 require('dotenv').config();
 
+const dbData =
+  process.env.MONGOOSE_USER === null || process.env.MONGOOSE_USER === ''
+    ? process.env.MONGOOSE_HOST
+    : 'mongodb+srv://' +
+      process.env.MONGOOSE_USER +
+      ':' +
+      process.env.MONGOOSE_PASSWORD +
+      '@' +
+      process.env.MONGOOSE_HOST +
+      '?retryWrites=true&w=majority';
+
 @Module({
   imports: [
     TrainingsModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://'
-      + process.env.MONGOOSE_USER +
-      ':'
-      + process.env.MONGOOSE_PASSWORD +
-      '@'
-      + process.env.MONGOOSE_HOST +
-      '?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      },
-    ),
+    MongooseModule.forRoot(dbData, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
