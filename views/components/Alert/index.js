39103b50f4fff9eced3e236419bfeda2c9bfa288
Krzysztof Bujak <bujak.krzysztof@outlook.com>
2020-06-16 11:02:54 +0200

    Update start script

--- views/components/Alert/index.js
+++ views/components/Alert/index.js
@@ -1,31 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './style.scss';
 import AppContext from '../../context';
 
-class Alert extends Component {
-  render() {
-    return (
-      <div
-        className="modal fade"
-        id="alert"
-        tabIndex="-1"
-        role="dialog"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-body">
-              <div id="red-toast" className="padding10">
-                <AppContext.Consumer>
-                  {(context) => <p>{context.alertText}</p>}
-                </AppContext.Consumer>
-              </div>
+const Alert = () => {
+  return (
+    <div className="modal fade" id="alert" tabIndex="-1" role="dialog" aria-hidden="true">
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-body">
+            <div id="red-toast">
+              <AppContext.Consumer>{context => <p>{context.alertText}</p>}</AppContext.Consumer>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Alert;
